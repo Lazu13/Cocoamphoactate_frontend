db3cf23c44310c3ed6e3e3b657612dcf59a663fe
Lazu13 <tomek.laz.13@gmail.com>
2016-12-17 17:50:58 +0100

    Users list and fixes

--- app/users/users.js
+++ app/users/users.js
@@ -9,7 +9,26 @@ angular.module('myApp.users', [
 
     .controller('UsersCtrl', function ($scope) {
 
-        $scope.person = {
-            person_id: 1
-        };
+        $scope.users = [
+            {
+                "id": 1,
+                "user_one": 3,
+                "name1": "tmp",
+                "email1": "tomasz@tomasz.tomasz",
+                "user_two": 4,
+                "name2": "tmp2",
+                "email2": "tomasz2@tomasz.tomasz"
+            },
+            {
+                "id": 2,
+                "user_one": 3,
+                "name1": "tmp",
+                "email1": "tomasz@tomasz.tomasz",
+                "user_two": 2,
+                "name2": "tmp3",
+                "email2": "tomasz3@tomasz.tomasz"
+            }
+        ];
+
+
     });

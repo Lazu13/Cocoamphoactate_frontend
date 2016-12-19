2fa8a14cd283773ff4c10eafb9ff80529904c5d5
Lazu13 <tomek.laz.13@gmail.com>
2016-12-19 11:17:14 +0100

    Communication with database v0.2

--- app/game/game.js
+++ app/game/game.js
@@ -6,7 +6,7 @@ angular.module('myApp.game', [
     'angular-input-stars'
 ])
 
-    .controller('GameCtrl', ['$scope', '$state', function ($scope, $state) {
+    .controller('GameCtrl', ['$scope', '$state', '$stateParams', '$http', function ($scope, $state, $stateParams, $http) {
         $scope.range = function (min, max, step) {
             step = step || 1;
             var input = [];
@@ -16,18 +16,37 @@ angular.module('myApp.game', [
             return input;
         };
 
-        $scope.sampleGame =
-            {
-                'id': 0,
-                'name': 'Wiedzmin 3: dziki gon',
-                'category': 'RPG',
-                'rating': 3,
-                'readNum': 100,
-                'readMoreText': "Read more",
-                'description': 'A witcher (or hexer) is someone who has undergone extensive training, ruthless mental and physical conditioning, and mysterious rituals (which take place at "witcher schools" such as Kaer Morhen) in preparation for becoming an itinerant monsterslayer for hire. Geralt, the central character in Andrzej Sapkowskis Witcher series and the subsequent games inspired by them, is said in the stories to be one of the greatest witchers; he is certainly legendary, but whether famous or infamous is more open to interpretation (and/or subject to gameplay, as the case may be).',
-                'image': 'http://vignette2.wikia.nocookie.net/wiedzmin/images/5/5d/Wiedzmin.jpg/revision/latest?cb=20130430183556'
-            };
+        /*
+         $scope.sampleGame =
+         {
+         'id': 0,
+         'name': 'Wiedzmin 3: dziki gon',
+         'category': 'RPG',
+         'rating': 3,
+         'readNum': 100,
+         'readMoreText': "Read more",
+         'description': 'A witcher (or hexer) is someone who has undergone extensive training, ruthless mental and physical conditioning, and mysterious rituals (which take place at "witcher schools" such as Kaer Morhen) in preparation for becoming an itinerant monsterslayer for hire. Geralt, the central character in Andrzej Sapkowskis Witcher series and the subsequent games inspired by them, is said in the stories to be one of the greatest witchers; he is certainly legendary, but whether famous or infamous is more open to interpretation (and/or subject to gameplay, as the case may be).',
+         'image': 'http://vignette2.wikia.nocookie.net/wiedzmin/images/5/5d/Wiedzmin.jpg/revision/latest?cb=20130430183556'
+         };
+         */
 
+        $scope.getGame = function () {
+            console.log($stateParams.gameId);
+            $http.get('http://127.0.0.1:8000/games/' + $stateParams.gameId, {
+                headers: {
+                    'Authorization': 'token a6047d9688d4505babebac76e116961ba56ff3eb',//'token ' + $cookies.get('Authorization'),
+                    'Content-Type': 'application/json'
+                }
+            })
+                .success(function (data) {
+                    //console.log(data);
+                    data.rating = Math.floor((Math.random() * 5) + 1);
+                    $scope.sampleGame = data;
+                })
+                .error(function () {
+                    $scope.sampleGame = [];
+                });
+        };
 
         $scope.comments = [
             {
@@ -40,8 +59,7 @@ angular.module('myApp.game', [
             }
         ];
 
-        $scope.clickHandler = function(starRating)
-        {
+        $scope.clickHandler = function (starRating) {
             $scope.starRating = starRating;
         };
 
--- app/users/users.js
+++ app/users/users.js
@@ -7,28 +7,48 @@ angular.module('myApp.users', [
 ])
 
 
-    .controller('UsersCtrl', function ($scope) {
+    .controller('UsersCtrl', function ($scope, $http) {
 
-        $scope.users = [
-            {
-                "id": 1,
-                "user_one": 3,
-                "name1": "tmp",
-                "email1": "tomasz@tomasz.tomasz",
-                "user_two": 4,
-                "name2": "tmp2",
-                "email2": "tomasz2@tomasz.tomasz"
-            },
-            {
-                "id": 2,
-                "user_one": 3,
-                "name1": "tmp",
-                "email1": "tomasz@tomasz.tomasz",
-                "user_two": 2,
-                "name2": "tmp3",
-                "email2": "tomasz3@tomasz.tomasz"
-            }
-        ];
+        /*
+         $scope.users = [
+         {
+         "id": 1,
+         "user_one": 3,
+         "name1": "tmp",
+         "email1": "tomasz@tomasz.tomasz",
+         "user_two": 4,
+         "name2": "tmp2",
+         "email2": "tomasz2@tomasz.tomasz"
+         },
+         {
+         "id": 2,
+         "user_one": 3,
+         "name1": "tmp",
+         "email1": "tomasz@tomasz.tomasz",
+         "user_two": 2,
+         "name2": "tmp3",
+         "email2": "tomasz3@tomasz.tomasz"
+         }
+         ];
+         */
 
+        $scope.getUsers = function () {
+            $http.get('http://127.0.0.1:8000/friends', {
+                headers: {
+                    'Authorization': 'token a6047d9688d4505babebac76e116961ba56ff3eb',//'token ' + $cookies.get('Authorization'),
+                    'Content-Type': 'application/json'
+                }
+            })
+                .success(function (data) {
+                    data.forEach(function (item) {
+                        item.name1 = item.user_one;
+                        item.name2 = item.user_two;
+                    });
+                    $scope.users = data;
+                })
+                .error(function () {
+                    $scope.users = [];
+                });
+        };
 
     });
